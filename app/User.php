706b585d85d26851227aae2450b00a37c3b5bf8d
Jakub Wójcik <wujek444@gmail.com>
2017-01-25 01:10:54 +0100

    Added registration form and done create_users_table migration.

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'imie', 'nazwisko', 'email', 'telefon', 'login', 'data-urodzenia', 'password',
     ];
 
     /**
