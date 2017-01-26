c08856583bbfae9f2a05789c98e61d9448b21b5d
Jakub Wójcik <wujek444@gmail.com>
2017-01-26 15:38:17 +0100

    Added hasRole and hasAnyRoles

--- app/User.php
+++ app/User.php
@@ -30,4 +30,27 @@ class User extends Authenticatable
     public function roles(){
         return $this->belongsToMany(Roles::class, 'roles_has_users', 'users_id', 'roles_id')->withTimestamps();
     }
+
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
 }
